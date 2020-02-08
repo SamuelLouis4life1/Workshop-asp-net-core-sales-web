0a068d274fbd0466b9f29458112785215756c686
Samuel Louis <Samuel Louis>
2020-02-08 18:09:52 -0300

    inicial commit in workshop asp.net core MVC Sales web application

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Email"] = "sampeur4life1@gmail.com/ samuel.louis2013@gmail.com / samuel.louis@al.infnet.edu.br";
+            ViewData["LinkInd"] = "https://www.linkedin.com/in/louis-samuel-544082110/";
+            ViewData["Github"] = "https://github.com/SamuelLouis4life1";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
